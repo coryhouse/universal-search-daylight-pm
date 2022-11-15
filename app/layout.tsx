f571f9e6ffd1c0825d3769fb4d727c90633dc9e6
Cory House <housecor@gmail.com>
2022-11-15 12:47:16 -0800

    Implement layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,38 @@
-import './globals.css'
+import "./globals.css";
+
+type SearchResult = {
+  proNumber: string;
+  weight: number;
+  pallets: number;
+  pieces: number;
+  pickupDate: string;
+  serviceDate: string;
+  shipmentTags: Array<string>;
+  status: string;
+};
+
+const searchResults: SearchResult[] = [
+  {
+    proNumber: "123456789",
+    weight: 100,
+    pallets: 1,
+    pieces: 1,
+    pickupDate: "2021-01-01",
+    serviceDate: "2021-01-01",
+    shipmentTags: ["tag1", "tag2"],
+    status: "status",
+  },
+];
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  function renderResults(result: SearchResult) {
+    return <div key={result.proNumber}>{result.proNumber}</div>;
+  }
+
   return (
     <html lang="en">
       {/*
@@ -12,7 +40,13 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div>
+          <input type="search" />
+          {searchResults.map(renderResults)}
+        </div>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
