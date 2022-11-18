c2e01a004f13eb6da07b6258c54594fb37f51554
Cory House <housecor@gmail.com>
2022-11-17 16:00:24 -0800

    Implement shipment details

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,6 @@ export default function Home() {
   const [search, setSearch] = useState("");
   const [shipments, setShipments] = useState<Shipment[]>([]);
 
-  // Keeping here merely for reference of how to do a fetch on initial load on the client.
-  // useEffect(() => {
-  //   async function fetchSearchResults() {
-  //     const response = await fetch("/api/shipments");
-  //     const results = await response.json();
-  //     setShipments(results);
-  //   }
-  //   fetchSearchResults();
-  //   // The empty dependency array [] means this effect will only run once
-  // }, []);
-
   // Deriving state
   const matchingShipments = shipments.filter((shipment) => {
     return shipment.proNumber.includes(search);
--- app/shipment-details/page.tsx
+++ app/shipment-details/page.tsx
@@ -1,16 +1,30 @@
 "use client";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { getShipments } from "../../services/shipmentsService";
+import { Shipment } from "../../types/Shipment.types";
 
 export default function ShipmentDetails() {
   const searchParams = useSearchParams();
+  const [shipment, setShipment] = useState<Shipment | null>(null);
 
   const proNumbers = searchParams.get("proNumbers");
   if (!proNumbers) throw new Error("No proNumbers provided");
   const proNumbersArray = proNumbers.split(",");
+  const currentProNumber = proNumbersArray[0];
+
+  useEffect(() => {
+    async function fetchSearchResults() {
+      // By convention, display the first shipment in the URL.
+      const res = await getShipments(currentProNumber);
+      setShipment(res[0]);
+    }
+    fetchSearchResults();
+    // The empty dependency array [] means this effect will only run once
+  }, [currentProNumber]);
 
   return (
     <>
-      <h1>Shipment Details</h1>
       <ul>
         {proNumbersArray.map((proNumber) => (
           <li key={proNumber}>
@@ -18,6 +32,14 @@ export default function ShipmentDetails() {
           </li>
         ))}
       </ul>
+
+      {shipment && (
+        <>
+          <h1>PRO #{shipment.proNumber}</h1>
+          <h2>Shipment Details</h2>
+          Total Pieces: {shipment.pieces}
+        </>
+      )}
     </>
   );
 }
