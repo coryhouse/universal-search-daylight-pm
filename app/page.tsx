26793f4498ec64c06ad047729b4cd3a667db72a4
Cory House <housecor@gmail.com>
2022-11-18 18:50:12 -0800

    Handle loading state

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import { getShipments } from "../services/shipmentsService";
 import { Shipment } from "../types/Shipment.types";
 import styles from "./page.module.css";
 import ShipmentResults from "./ShipmentResults";
+import { Toaster } from "react-hot-toast";
+import Spinner from "./Spinner";
 
 export default function Home() {
   const [search, setSearch] = useState("");
   const [shipments, setShipments] = useState<Shipment[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Deriving state
   const matchingShipments = shipments.filter((shipment) => {
@@ -16,7 +19,7 @@ export default function Home() {
 
   function renderResults() {
     if (!search) return null;
-
+    if (isLoading) return <Spinner />;
     if (matchingShipments.length > 0) {
       return <ShipmentResults matchingShipments={matchingShipments} />;
     } else {
@@ -26,6 +29,7 @@ export default function Home() {
 
   return (
     <div>
+      <Toaster />
       <input
         placeholder="Enter proNumber"
         type="search"
@@ -33,7 +37,9 @@ export default function Home() {
           setSearch(event.target.value);
           if (event.target.value) {
             // NOTE: Must use event.target.value here because setSearch is async, and thus, not updated yet.
+            setIsLoading(true);
             const results = await getShipments(event.target.value);
+            setIsLoading(false);
             setShipments(results);
           } else {
             setShipments([]);
